9dff0e237c83e738b5aafc592bc7bad3aa15d435
djeasy84 <lbaglioni84@gmail.com>
2018-07-20 20:22:05 +0200

    Aggiunto supporto devices per easyHome

--- Libraries/W5500.h
+++ Libraries/W5500.h
@@ -87,7 +87,7 @@ bool IPAddress::PortToBytes(const uint16_t port, uint8_t *bytes)
 class Ethernet
 {
     public:
-        int setup(uint8_t ss /* ARDUINO_PIN_10 */, const char *mac = "00:11:22:33:44:55", const char *ip = "192.168.1.100", const char *netmask = "255.255.255.0", const char *gateway = "0.0.0.0");
+        bool setup(uint8_t ss /* ARDUINO_PIN_10 */, const char *mac = "00:11:22:33:44:55", const char *ip = "192.168.1.100", const char *netmask = "255.255.255.0", const char *gateway = "0.0.0.0");
 
         bool open(const char *ip, uint16_t port, uint8_t type = TCP, uint16_t lPort = 0, uint8_t mode = CLIENT);
         unsigned int available();
@@ -118,7 +118,7 @@ Ethernet ETH;
 
 /****************************************************************************************/
 
-int Ethernet::setup(uint8_t ss, const char *mac, const char *ip, const char *netmask, const char *gateway)
+bool Ethernet::setup(uint8_t ss, const char *mac, const char *ip, const char *netmask, const char *gateway)
 {
     incPort = 0;
     sockType = NAT;
@@ -159,6 +159,10 @@ int Ethernet::setup(uint8_t ss, const char *mac, const char *ip, const char *net
 
 bool Ethernet::open(const char *ip, uint16_t port, uint8_t type, uint16_t lPort, uint8_t mode)
 {
+    uint8_t bytePHY = 0x00;
+    getRegistry(SYS_CM, 0x002E, &bytePHY);
+    if ((bytePHY & 0b00000001) == 0b00000000)
+        return false;
     incPort = lPort;
     sockType = type;
     if (mode == CLIENT)
@@ -199,28 +203,27 @@ bool Ethernet::open(const char *ip, uint16_t port, uint8_t type, uint16_t lPort,
             }
             return true;
         }
-        /*else if (sockType == UDP)
+        else if (sockType == UDP)
         {
             close();
-            if (!setRegistry(1, 0x0000, 0b00000010))
-                return 1;
+            if (!setRegistry(SOCK_CM, 0x0000, 0b00000010))
+                return false;
             uint8_t bytesSrcPort[2] = {0, 0};
-            IPAddress::PortToBytes(getSourcePort(), bytesSrcPort);
-            if (!setRegistry(1, 0x0004, 2, bytesSrcPort))
-                return 2;
-            setRegistry(1, 0x0001, 0x01);
+            IPAddress::PortToBytes(65000, bytesSrcPort);
+            if (!setRegistry(SOCK_CM, 0x0004, 2, bytesSrcPort))
+                return false;
+            setRegistry(SOCK_CM, 0x0001, 0x01);
             uint8_t byteStatus = 0x00;
-            getRegistry(1, 0x0003, &byteStatus);
-            SHW0.println(byteStatus);
+            getRegistry(SOCK_CM, 0x0003, &byteStatus);
             if (byteStatus != 0x22)
             {
                 close();
-                return 3;
+                return false;
             }
             IPAddress::IPToBytes(ip, destIPUDP);
             IPAddress::PortToBytes(port, destPortUDP);
-            return 4;
-        }*/
+            return true;
+        }
         return false;
     }
     else if (mode == SERVER)
@@ -234,18 +237,29 @@ unsigned int Ethernet::available()
 {
     if (sockType == TCP)
     {
-        uint16_t rx_data_len = 0x0000;
+        uint16_t rx_data_len = 0;
         getRegistry(SOCK_CM, 0x0026, (uint8_t *)&rx_data_len+1);
         getRegistry(SOCK_CM, 0x0027, (uint8_t *)&rx_data_len+0);
         return rx_data_len;
     }
-    /*else if (sockType == UDP)
+    else if (sockType == UDP)
     {
-        uint16_t rx_data_len = 0x0000;
-        getRegistry(0x0426, (uint8_t *)&rx_data_len+1);
-        getRegistry(0x0427, (uint8_t *)&rx_data_len+0);
-        return (rx_data_len < 8)?0:(rx_data_len  8);
-    }*/
+        uint16_t rx_data_len = 0;
+        getRegistry(SOCK_CM, 0x0026, (uint8_t *)&rx_data_len+1);
+        getRegistry(SOCK_CM, 0x0027, (uint8_t *)&rx_data_len+0);
+        if (rx_data_len >= 8)
+        {
+            uint16_t rx_rd = 0x0000;
+            getRegistry(SOCK_CM, 0x0028, (uint8_t *)&rx_rd+1);
+            getRegistry(SOCK_CM, 0x0029, (uint8_t *)&rx_rd+0);
+            uint16_t rx_offset = rx_rd & 0x3FFF;
+            uint16_t rx_mem_address = 0x0000+rx_offset;
+            getRegistry(SOCK_RX, rx_mem_address+6, 1, (uint8_t *)&rx_data_len+1);
+            getRegistry(SOCK_RX, rx_mem_address+7, 1, (uint8_t *)&rx_data_len+0);
+            return rx_data_len;
+        }
+        return 0;
+    }
     return 0;
 }
 
@@ -253,6 +267,13 @@ bool Ethernet::read(uint16_t len, uint8_t *data)
 {
     if (sockType == TCP)
     {
+		uint8_t byteStatus = 0x00;
+		getRegistry(SOCK_CM, 0x0003, &byteStatus);
+		if (byteStatus != 0x17)
+		{
+			close();
+			return false;
+		}
         uint16_t rx_size = available();
         if (rx_size == 0)
             return false;
@@ -263,7 +284,7 @@ bool Ethernet::read(uint16_t len, uint8_t *data)
         getRegistry(SOCK_CM, 0x0029, (uint8_t *)&rx_rd+0);
         uint16_t rx_offset = rx_rd & 0x3FFF;
         uint16_t rx_mem_address = 0x0000+rx_offset;
-        if (rx_offset + len > 16384)
+        if (rx_offset+len > 16384)
         {
             uint16_t rx_size_last = 16384-rx_offset;
             getRegistry(SOCK_RX, rx_mem_address, rx_size_last, data);
@@ -279,72 +300,69 @@ bool Ethernet::read(uint16_t len, uint8_t *data)
         setRegistry(SOCK_CM, 0x0001, 0x40);
         return true;
     }
-    /*else if (sockType == UDP)
+    else if (sockType == UDP)
     {
-        uint16_t rx_size = available() + 8;
+		uint8_t byteStatus = 0x00;
+		getRegistry(SOCK_CM, 0x0003, &byteStatus);
+		if (byteStatus != 0x22)
+		{
+			close();
+			return false;
+		}
+        uint16_t rx_size = available();
         if (rx_size == 0)
             return false;
-        if (rx_size  8 < len)
+        if (rx_size < len)
             return false;
         uint16_t rx_rd = 0x0000;
-        getRegistry(0x0428, (uint8_t *)&rx_rd+1);
-        getRegistry(0x0429, (uint8_t *)&rx_rd+0);
-        uint16_t rx_offset = rx_rd & 0x1FFF;
-        uint16_t rx_mem_address = 0x6000 + rx_offset;
-        uint8_t header_data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-        if (rx_offset + 8 > 8192)
+        getRegistry(SOCK_CM, 0x0028, (uint8_t *)&rx_rd+1);
+        getRegistry(SOCK_CM, 0x0029, (uint8_t *)&rx_rd+0);
+        uint16_t rx_offset = rx_rd & 0x3FFF;
+        uint16_t rx_mem_address = 0x0000+rx_offset+8;
+        if (rx_offset+rx_size > 16384)
         {
-            uint16_t rx_size_last = 8192  rx_offset;
-            getRegistry(rx_mem_address, rx_size_last, header_data);
-            getRegistry(0x6000, 8  rx_size_last, header_data+rx_size_last);
-            rx_offset = 8  rx_size_last;
-        }
-        else
-        {
-            getRegistry(rx_mem_address, 8, header_data);
-            rx_offset += 8;
-        }
-        uint16_t rx_packet_size = (uint16_t)((header_data[6]<<8)|(header_data[7]&0x00FF));
-        rx_mem_address = 0x6000 + rx_offset;
-        if (rx_offset + rx_packet_size > 8192)
-        {
-            uint16_t rx_size_last = 8192  rx_offset;
+            uint16_t rx_size_last = 16384-rx_offset;
             getRegistry(rx_mem_address, rx_size_last, data);
-            getRegistry(0x6000, rx_packet_size  rx_size_last, data+rx_size_last);
+            getRegistry(SOCK_RX, 0x0000, rx_size-rx_size_last, data+rx_size_last);
         }
         else
         {
-            getRegistry(rx_mem_address, rx_packet_size, data);
+            getRegistry(SOCK_RX, rx_mem_address, len, data);
         }
-        rx_rd += 8 + rx_packet_size;
-        setRegistry(0x0428, (uint8_t)(rx_rd >> 8));
-        setRegistry(0x0429, (uint8_t)(rx_rd & 0x00FF));
-        setRegistry(0x0401, 0x40);
+        rx_rd += 8+rx_size;
+        setRegistry(SOCK_CM, 0x0028, (uint8_t)(rx_rd >> 8));
+        setRegistry(SOCK_CM, 0x0029, (uint8_t)(rx_rd & 0x00FF));
+        setRegistry(SOCK_CM, 0x0001, 0x40);
         return true;
-    }*/
+    }
     return false;
 }
 
 bool Ethernet::write(uint16_t len, uint8_t *data)
 {
-   if (sockType == TCP)
+	if (sockType == TCP)
     {
+		uint8_t byteStatus = 0x00;
+		getRegistry(SOCK_CM, 0x0003, &byteStatus);
+		if (byteStatus != 0x17)
+		{
+            close();
+			return false;
+		}
         if (len > 16384)
             return false;
         uint16_t tx_size = 0x0000;
         getRegistry(SOCK_CM, 0x0020, (uint8_t *)&tx_size+1);
         getRegistry(SOCK_CM, 0x0021, (uint8_t *)&tx_size+0);
-        while (tx_size < len)
-        {
-            getRegistry(SOCK_CM, 0x0020, (uint8_t *)&tx_size+1);
-            getRegistry(SOCK_CM, 0x0021, (uint8_t *)&tx_size+0);
-        }
+        if (tx_size < len)
+			return false;
+        setRegistry(SOCK_CM, 0x0002, 0b00011000);
         uint16_t tx_wr = 0x0000;
         getRegistry(SOCK_CM, 0x0024, (uint8_t *)&tx_wr+1);
         getRegistry(SOCK_CM, 0x0025, (uint8_t *)&tx_wr+0);
         uint16_t tx_offset = tx_wr & 0x3FFF;
         uint16_t tx_mem_address = 0x0000+tx_offset;
-        if (tx_offset + len > 16384)
+        if (tx_offset+len > 16384)
         {
             uint16_t tx_size_last = 16384-tx_offset;
             setRegistry(SOCK_TX, tx_mem_address, tx_size_last, data);
@@ -362,9 +380,16 @@ bool Ethernet::write(uint16_t len, uint8_t *data)
         getRegistry(SOCK_CM, 0x0002, &byteInterrupt);
         while ((byteInterrupt & 0b00010000) != 0b00010000)
         {
-            uint8_t byteStatus = 0x00;
+            byteStatus = 0x00;
             getRegistry(SOCK_CM, 0x0003, &byteStatus);
             if (byteStatus == 0x00)
+            {
+                close();
+                return false;
+            }
+            byteInterrupt = 0x00;
+			getRegistry(SOCK_CM, 0x0002, &byteInterrupt);
+            if ((byteInterrupt & 0b00001000) != 0b00001000)
             {
                 close();
                 return false;
@@ -372,73 +397,64 @@ bool Ethernet::write(uint16_t len, uint8_t *data)
         }
         return true;
     }
-    /*else if (sockType == UDP)
+    else if (sockType == UDP)
     {
-        if (len > 8192)
+		uint8_t byteStatus = 0x00;
+		getRegistry(SOCK_CM, 0x0003, &byteStatus);
+		if (byteStatus != 0x22)
+		{
+			close();
+			return false;
+		}
+        if (len > 16384)
             return false;
         uint16_t tx_size = 0x0000;
-        getRegistry(0x0420, (uint8_t *)&tx_size+1);
-        getRegistry(0x0421, (uint8_t *)&tx_size+0);
+        getRegistry(SOCK_CM, 0x0020, (uint8_t *)&tx_size+1);
+        getRegistry(SOCK_CM, 0x0021, (uint8_t *)&tx_size+0);
         while (tx_size < len)
         {
-            getRegistry(0x0420, (uint8_t *)&tx_size+1);
-            getRegistry(0x0421, (uint8_t *)&tx_size+0);
+            getRegistry(SOCK_CM, 0x0020, (uint8_t *)&tx_size+1);
+            getRegistry(SOCK_CM, 0x0021, (uint8_t *)&tx_size+0);
         }
-        setRegistry(0x040C, 4, destIPUDP);
-        setRegistry(0x0410, 2, destPortUDP);
+        setRegistry(SOCK_CM, 0x000C, 4, destIPUDP);
+        setRegistry(SOCK_CM, 0x0010, 2, destPortUDP);
         uint16_t tx_wr = 0x0000;
-        getRegistry(0x0424, (uint8_t *)&tx_wr+1);
-        getRegistry(0x0425, (uint8_t *)&tx_wr+0);
-        uint16_t tx_offset = tx_wr & 0x1FFF;
-        uint16_t tx_mem_address = 0x4000 + tx_offset;
-        if (tx_offset + len > 8192)
+        getRegistry(SOCK_CM, 0x0024, (uint8_t *)&tx_wr+1);
+        getRegistry(SOCK_CM, 0x0025, (uint8_t *)&tx_wr+0);
+        uint16_t tx_offset = tx_wr & 0x3FFF;
+        uint16_t tx_mem_address = 0x0000+tx_offset;
+        if (tx_offset+len > 16384)
         {
-            uint16_t tx_size_last = 8192  tx_offset;
-            setRegistry(tx_mem_address, tx_size_last, data);
-            setRegistry(0x4000, len  tx_size_last, data+tx_size_last);
+            uint16_t tx_size_last = 16384-tx_offset;
+            setRegistry(SOCK_TX, tx_mem_address, tx_size_last, data);
+            setRegistry(SOCK_TX, 0x0000, len-tx_size_last, data+tx_size_last);
         }
         else
         {
-            setRegistry(tx_mem_address, len, data);
+            setRegistry(SOCK_TX, tx_mem_address, len, data);
         }
         tx_wr += len;
-        setRegistry(0x0424, (uint8_t)(tx_wr >> 8));
-        setRegistry(0x0425, (uint8_t)(tx_wr & 0x00FF));
-        setRegistry(0x0401, 0x20);
-        uint8_t byteStatus = 0x00;
-        getRegistry(0x0401, &byteStatus);
-        while (byteStatus);
+        setRegistry(SOCK_CM, 0x0024, (uint8_t)(tx_wr >> 8));
+        setRegistry(SOCK_CM, 0x0025, (uint8_t)(tx_wr & 0x00FF));
+        setRegistry(SOCK_CM, 0x0001, 0x20);
         return true;
-    }*/
+    }
     return false;
 }
 
 bool Ethernet::close()
 {
     if (sockType == TCP)
-    {
         setRegistry(SOCK_CM, 0x0001, 0x08);
-        uint8_t byte = 0x00;
-        getRegistry(SOCK_CM, 0x0003, &byte);
-        while (byte != 0x00)
-            getRegistry(1, 0x0003, &byte);
+    else if (sockType == UDP)
         setRegistry(SOCK_CM, 0x0001, 0x10);
-        byte = 0x00;
-        getRegistry(SOCK_CM, 0x0003, &byte);
-        while (byte != 0x00)
-            getRegistry(SOCK_CM, 0x0003, &byte);
-        return true;
-    }
-    /*else if (sockType == UDP)
-    {
-        setRegistry(1, 0x0001, 0x10);
-        uint8_t byte = 0x00;
+    else
+        return false;
+    uint8_t byte = 0x00;
+    getRegistry(SOCK_CM, 0x0003, &byte);
+    while (byte != 0x00)
         getRegistry(1, 0x0003, &byte);
-        while (byte != 0x00)
-            getRegistry(1, 0x0003, &byte);
-        return true;
-    }*/
-    return false;
+    return true;
 }
 
 void Ethernet::reset()
